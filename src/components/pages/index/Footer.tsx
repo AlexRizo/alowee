ff78a17a8ce380d556e5b8b0111be51a40d400b3
Alex <david_alejandrorizo@outlook.com>
2025-07-22 10:18:42 -0600

    footer fixed

--- src/components/pages/index/Footer.tsx
+++ src/components/pages/index/Footer.tsx
@@ -3,16 +3,17 @@ import { Mail, MessageCircle } from "lucide-react"
 export const Footer = ({ hidden = true }: { hidden: boolean }) => {
   return (
     <footer
-      className="bg-black border-t border-gray-800 py-6 flex justify-evenly items-center absolute bottom-0 w-full transition-opacity"
+      className="bg-black border-t border-gray-800 py-6 flex lg:flex-row flex-col lg:gap-0 gap-1 justify-evenly items-center absolute bottom-0 w-full transition-opacity"
       style={{ opacity: hidden ? 0 : 1 }}
     >
-      <a href="/">
+      <a href="/" className="lg:order-none order-1">
         <img src="/img/alowee-logo-morado.webp" className="w-23 h-auto"/>
       </a>
-      <p className="text-gray-400">©2025 Hecho con ♥ por Alowee Interactive</p>
-      <div className="flex gap-4">
+      <p className="text-gray-400 lg:order-none order-3">©2025 Hecho con ♥ por Alowee Interactive</p>
+      <div className="flex gap-4 lg:order-none order-2">
         <MessageCircle className="size-6 text-gray-400" />
         <Mail className="size-6 text-gray-400" />
+        <small className="text-gray-400">34°38° ´N</small>
       </div>
     </footer>
   )
